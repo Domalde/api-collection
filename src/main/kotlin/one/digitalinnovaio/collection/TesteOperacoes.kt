a49bed16ffa2936a188370fabe664f09d1e28839
Léo Monteiro <78476854+Domalde@users.noreply.github.com>
2022-01-21 13:27:44 -0300

    update teste operation array of test class

--- src/main/kotlin/one/digitalinnovaio/collection/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovaio/collection/TesteOperacoes.kt
@@ -1,7 +1,5 @@
 package one.digitalinnovaio.collection
 
-import kotlin.math.max
-
 fun main(){
     val salarios = doubleArrayOf(1000.0, 2250.0,4000.0)
 
@@ -9,14 +7,21 @@ fun main(){
         println(i)
     }
     println("_______________")
-    //println("Maior salário: ${salarios.max()}")
-   // println("Menor salário: ${salarios.min()}")
+    println("Maior salário: ${salarios.maxOf {it}}")
+    println("Menor salário: ${salarios.minOf {it}}")
     println("Média salário: ${salarios.average()}")
 
     val salariosMaior2500 = salarios.filter{it > 2500.0}
     println("_______________")
 
     salariosMaior2500.forEach {println(it)}
+    println("_______________")
+    println(salarios.count {it in 2000.0..5000.0})
+    println(salarios.find {it == 1000.0})
+    println(salarios.any {it == 1000.0})
+
+
+
 
 
 }
